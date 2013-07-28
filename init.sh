62e66e2577346a871d11b836098449a4b045ea22
Marcos Vives Del Sol <socram8888@gmail.com>
2013-07-28 18:17:17 +0200

    Do not remove "base" folder

--- init.sh
+++ init.sh
@@ -3,7 +3,17 @@
 ORIG_PWD="$(pwd)"
 cd "$(dirname "$0")"
 
-rm -Rf base build &> /dev/null
-git submodule update --init
+if !([ -d base ]); then
+    git submodule update --init
+fi
+
+if [ -d build ]; then
+    rm build/* &> /dev/null
+    if !(rmdir build); then
+        echo "Unable to remove build directory"
+        cd "$ORIG_PWD"
+        exit $?
+    fi
+fi
 
 cd "$ORIG_PWD"
